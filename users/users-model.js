f86f971970a6179e8658379d8cc22774cfbff207
Farid Hamida <faridhamida22@gmail.com>
2020-07-10 10:30:11 -0700

    First commit

---
+++ users/users-model.js
@@ -0,0 +1,28 @@
+const db = require("../database/db-config");
+
+function find() {
+    return db("users").select("id", "email")
+}
+
+function findBy(filter) {
+    return db("users").where(filter);
+}
+
+async function add(user) {
+    return db("users").insert(user);
+}
+
+async function findByEmail(email){
+    const user = await db("users")
+    .where({ email })
+    .first();
+
+    return user ? user : null;
+}
+
+module.exports = {
+    add,
+    find,
+    findBy,
+    findByEmail
+};
