a7b81750086c239d6dff6158a0e08fdfb7a83e22
Farid Hamida <faridhamida22@gmail.com>
2020-08-07 10:55:22 -0700

    testing

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const server = require("./api/server");
-// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+server.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
 module.exports = server;
--- knexfile.js
+++ knexfile.js
@@ -9,21 +9,19 @@ module.exports = {
     }
   },
 
-  // production: {
-  //   client: 'sqlite3',
-  //   connection: {
+  production: {
+    client: 'sqlite3',
+    connection: {
       
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+      database: 'job-saver'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 
 };
