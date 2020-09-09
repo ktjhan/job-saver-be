91686c89f7af1a1224ff6b7b20209743ca859cf4
Farid Hamida <faridhamida22@gmail.com>
2020-09-09 14:10:32 -0700

    checking errors

--- middleware/oktaJwtVerifier.js
+++ middleware/oktaJwtVerifier.js
@@ -20,6 +20,7 @@ function authenticationRequired(req, res, next) {
   let match = authHeader.match(/Bearer (.+)/);
   let matcharr = match.split()
   match = matcharr.slice(6, matcharr.length)
+  console.log(match)
   if (!match) {
     console.log("error at oktaJwtVerifier L 23")
     return res
