4198cb21dc9cd12ca8ecce11695cec2b92916368
Farid Hamida <faridhamida22@gmail.com>
2020-09-09 14:32:30 -0700

    reverting changes

--- middleware/oktaJwtVerifier.js
+++ middleware/oktaJwtVerifier.js
@@ -1,47 +1,47 @@
-// require("dotenv").config()
-// const OktaJwtVerifier = require("@okta/jwt-verifier");
+require("dotenv").config()
+const OktaJwtVerifier = require("@okta/jwt-verifier");
 
 
-// const oktaJwtVerifier = new OktaJwtVerifier({
-//   issuer: process.env.OKTA_ORG,
-//   clientId: process.env.OKTA_CLIENT_ID,
-//   assertClaims: {
-//     aud: "api://default"
-//   }
-// });
+const oktaJwtVerifier = new OktaJwtVerifier({
+  issuer: process.env.OKTA_ORG,
+  clientId: process.env.OKTA_CLIENT_ID,
+  assertClaims: {
+    aud: "api://default"
+  }
+});
 
-// function authenticationRequired(req, res, next) {
-//   // console.log('hit')
-//   if(process.env.NODE_ENV === 'development') {
-//     return next();
-//   }
+function authenticationRequired(req, res, next) {
+  // console.log('hit')
+  if(process.env.NODE_ENV === 'development') {
+    return next();
+  }
   
-//   const authHeader = req.headers.authorization || "";
-//   let match = authHeader.match(/Bearer (.+)/);
-//   let matcharr = match.split()
-//   match = matcharr.slice(6, matcharr.length)
-//   console.log(match)
-//   if (!match) {
-//     console.log("error at oktaJwtVerifier L 23")
-//     return res
-//       .status(401)
-//       .json({ message: "Please try logging in first, then try again!" });
-//   }
+  const authHeader = req.headers.authorization || "";
+  let match = authHeader.match(/Bearer (.+)/);
+  let matcharr = match.split()
+  match = matcharr.slice(6, matcharr.length)
+  console.log(match)
+  if (!match) {
+    console.log("error at oktaJwtVerifier L 23")
+    return res
+      .status(401)
+      .json({ message: "Please try logging in first, then try again!" });
+  }
 
-//   const accessToken = match[1];
-//   const expectedAudience = "api://default";
+  const accessToken = match[1];
+  const expectedAudience = "api://default";
 
-//   return oktaJwtVerifier
-//     .verifyAccessToken(accessToken, expectedAudience)
-//     .then(jwt => {
+  return oktaJwtVerifier
+    .verifyAccessToken(accessToken, expectedAudience)
+    .then(jwt => {
     
-//       req.jwt = jwt;
-//       next();
-//     })
-//     .catch(err => {
-//       res.status(401).json(err.message);
-//       console.log(err);
-//     });
-// }
+      req.jwt = jwt;
+      next();
+    })
+    .catch(err => {
+      res.status(401).json(err.message);
+      console.log(err);
+    });
+}
 
-// module.exports = authenticationRequired;
+module.exports = authenticationRequired;
