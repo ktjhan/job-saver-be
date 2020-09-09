4aa2b9909a1e534f376631e0db7d7b0f95bf946f
Farid Hamida <faridhamida22@gmail.com>
2020-09-09 14:14:26 -0700

    testing

--- api/server.js
+++ api/server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet")
 const jobsRouter = require("../jobs/jobs-router")
-const authenticationRequired = require('../middleware/oktaJwtVerifier')
-const checkUser = require("../middleware/checkUser");
+// const authenticationRequired = require('../middleware/oktaJwtVerifier')
+// const checkUser = require("../middleware/checkUser");
 
 const server = express();
 
 server.use(cors());
 server.use(helmet());
 server.use(express.json());
-server.use("/users", authenticationRequired, checkUser, jobsRouter)
+server.use("/users", jobsRouter)
 
 // Connection test 
 server.get("/", (req, res, next) => {
